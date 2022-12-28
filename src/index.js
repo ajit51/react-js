7e07e7a6877aaf7be2036334e50c19026783e3ae
ajit51 <ajitk433@gmail.com>
2022-12-28 16:00:16 +0530

    Render Multiple Elements inside ReactDOM.render() in ReactJS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// In react version 16 it,s possible for render() to return an array of elements.
+
 ReactDOM.render(
-  <h1>Ajit Kumar</h1>,
+  /* <div>
+  <h1>Thapa Technical</h1>
+  <p>Plz like and subscribe</p>
+  <h1>Plz cubscribe my channel</h1>
+  </div> */
+  
+ /* [
+  <h1>Thapa Technical</h1>,
+  <p>Plz like and subscribe</p>,
+  <h1>Plz cubscribe my channel</h1>
+] */
+
+<>
+  <h1>Thapa Technical</h1>
+  <p>Plz like and subscribe</p>
+  <h1>Plz cubscribe my channel</h1>
+  </>
+  ,
   document.getElementById("root")
 )
