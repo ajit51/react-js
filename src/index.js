1a9e77534c45df94b5e877b8aa49e322ced41d61
ajit51 <ajitk433@gmail.com>
2022-12-28 16:06:44 +0530

    Understanding React Fragment in React JS

--- src/index.js
+++ src/index.js
@@ -4,23 +4,11 @@ import ReactDOM from 'react-dom';
 // In react version 16 it,s possible for render() to return an array of elements.
 
 ReactDOM.render(
-  /* <div>
+<React.Fragment>
   <h1>Thapa Technical</h1>
   <p>Plz like and subscribe</p>
   <h1>Plz cubscribe my channel</h1>
-  </div> */
-  
- /* [
-  <h1>Thapa Technical</h1>,
-  <p>Plz like and subscribe</p>,
-  <h1>Plz cubscribe my channel</h1>
-] */
-
-<>
-  <h1>Thapa Technical</h1>
-  <p>Plz like and subscribe</p>
-  <h1>Plz cubscribe my channel</h1>
-  </>
+  </React.Fragment>
   ,
   document.getElementById("root")
 )
