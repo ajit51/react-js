b7cce1b1afe9562ed2d139777c2cfe23d7eb3fda
ajit51 <ajitk433@gmail.com>
2022-12-28 16:48:04 +0530

    ES6 Template Literals in JSX in React JS

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import ReactDOM from 'react-dom';
 const fName = "Ajit";
 const lName = "Kumar";
 
-const currDate = new Date().toLocaleDateString();
-const currTime = new Date().toLocaleTimeString();
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+
+const links = "https://www.thapatechnical.com/";
 
 ReactDOM.render(
 <>
-  <h1> { `Hello, My name is ${ fName } ${lName}`}</h1>
-  <h1>Current Date is = {currDate}</h1>
-  <h1>Current Time is = {currTime}</h1>
+  <h1 contentEditable="true"> { `Hello, My name is ${ fName } ${lName}`}</h1>
+  <img src={img1} alt="random images1"/>
+  <img src={img2} alt="random images2"/>
+  <a href={links} target="_blank">
+  <img src={img3} alt="random images3" />
+  </a>
   </>
   ,
   document.getElementById("root")
